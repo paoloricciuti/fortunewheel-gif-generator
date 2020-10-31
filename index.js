ccfc77808628d10c773f0dde14c9adf1ff67b0ed
Paolo Ricciuti <ricciutipaolo@gmail.com>
2020-10-31 10:55:13 +0100

    Fix port

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-    response.json({ok: true});
+    response.json({ ok: true });
 });
-const listener = app.listen(process.env.PORT, () => {
+const listener = app.listen(process.env.PORT || 3000, () => {
     console.log("Your app is listening on port " + listener.address().port);
-  });
-  
+});
